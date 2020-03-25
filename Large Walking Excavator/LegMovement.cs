b1707998f81de5418e4ec5cb76ed616b246cd288
Lars Pidde <lars.pidde@gmail.com>
2020-03-25 18:14:10 +0100

    initial commit

---
+++ Large Walking Excavator/LegMovement.cs
@@ -0,0 +1,167 @@
+﻿using Sandbox.Game.EntityComponents;
+using Sandbox.ModAPI.Ingame;
+using Sandbox.ModAPI.Interfaces;
+using SpaceEngineers.Game.ModAPI.Ingame;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System;
+using VRage.Collections;
+using VRage.Game.Components;
+using VRage.Game.GUI.TextPanel;
+using VRage.Game.ModAPI.Ingame.Utilities;
+using VRage.Game.ModAPI.Ingame;
+using VRage.Game.ObjectBuilders.Definitions;
+using VRage.Game;
+using VRage;
+using VRageMath;
+
+namespace IngameScript
+{
+    partial class Program
+    {
+        abstract public class LegMovement
+        {
+            abstract public bool Update();  //true means continue these Updates
+        }
+        
+        public class UnlockSafe : LegMovement
+        {
+            public UnlockSafe(IMyLandingGear landingGearToUnlock, IMyLandingGear landingGearControl)  // to make sure at least one landing gear is always locked to the ground
+            {
+                if (landingGearControl.IsLocked)
+                {
+                    landingGearToUnlock.Unlock();
+                }
+            }
+
+            public override bool Update()
+            {
+                return false;
+            }
+        }
+
+        public class PistonAndLandingGearExtendAndLock : LegMovement
+        {
+            private readonly float pistonVelocity = 1; //unit: m/s
+            private readonly IMyPistonBase piston;
+            private readonly IMyLandingGear landingGear;
+           
+            public PistonAndLandingGearExtendAndLock(IMyPistonBase piston, IMyLandingGear landingGear)
+            {
+                this.piston = piston;
+                this.landingGear = landingGear;
+                this.piston.Velocity = pistonVelocity;
+
+                this.StartMovement();
+            }
+
+            private void StartMovement()
+            {
+                this.piston.ApplyAction("OnOff_On");
+                this.piston.ApplyAction("Extend");
+            }
+
+            public override bool Update()
+            {
+                if (landingGear.LockMode == LandingGearMode.ReadyToLock)
+                {
+                    this.piston.ApplyAction("OnOff_Off");
+                    this.landingGear.Lock();
+                    return false;
+                }
+                else return true;
+            }
+        }
+
+        public class PistonMoveFull : LegMovement
+        {
+            private readonly float velocity = 1;  //unit: m/s
+            private readonly bool extend;
+            private readonly IMyPistonBase piston;
+           
+            public PistonMoveFull(IMyPistonBase piston, bool extend)
+            {
+                this.piston = piston;
+                this.extend = extend;
+                this.piston.Velocity = velocity;
+
+                this.StartMovement();
+            }
+
+            private void StartMovement()
+            {
+                if (this.extend)
+                {
+                    this.piston.ApplyAction("Extend");
+                }
+                else
+                {
+                    this.piston.ApplyAction("Retract");
+                }
+                this.piston.ApplyAction("OnOff_On");
+            }
+
+            public override bool Update()
+            {
+                //check if end reached
+                if (this.extend && this.piston.Status == PistonStatus.Extended ||
+                    !this.extend && this.piston.Status == PistonStatus.Retracted)
+                {
+                    this.piston.ApplyAction("OnOff_Off");
+                    return false;
+                }
+                else return true;
+            }
+        }
+
+        public class RotorPositionSet : LegMovement
+        {
+            private readonly float rotationSpeed = 1;  //unit: RPM
+            private readonly double targetAngle;
+            private readonly IMyMotorStator rotor;
+            
+            public RotorPositionSet(IMyMotorStator rotor, double targetAngle)
+            {
+                this.targetAngle = targetAngle;
+                this.rotor = rotor;
+
+                this.StartMovement();
+            }
+
+            private void StartMovement()
+            {
+                //set speed (and direction)
+                if (this.targetAngle - this.rotor.Angle > 0 && this.targetAngle - this.rotor.Angle < Math.PI ||
+                    this.targetAngle - this.rotor.Angle < 0 && this.rotor.Angle - this.targetAngle > Math.PI)
+                {
+                    this.rotor.TargetVelocityRPM = rotationSpeed;
+                }
+                else
+                {
+                    this.rotor.TargetVelocityRPM = -1 * rotationSpeed;
+                }
+                this.rotor.ApplyAction("OnOff_On");
+                this.rotor.RotorLock = false;
+            }
+
+            override public bool Update()
+            {
+                //stop movement if necessary
+                if (this.rotor.TargetVelocityRPM > 0 && this.targetAngle - this.rotor.Angle > 0 && this.targetAngle - this.rotor.Angle > 1.5 * Math.PI ||
+                    this.rotor.TargetVelocityRPM > 0 && this.targetAngle - this.rotor.Angle < 0 && this.rotor.Angle - this.targetAngle < 0.5 * Math.PI ||
+                    this.rotor.TargetVelocityRPM < 0 && this.targetAngle - this.rotor.Angle > 0 && this.targetAngle - this.rotor.Angle < 0.5 * Math.PI ||
+                    this.rotor.TargetVelocityRPM < 0 && this.targetAngle - this.rotor.Angle < 0 && this.rotor.Angle - this.targetAngle > 1.5 * Math.PI)
+                {
+                    //stop and lock rotor
+                    this.rotor.ApplyAction("OnOff_Off");
+                    this.rotor.RotorLock = true;
+                    return false;
+                }
+                else
+                    return true;
+            }
+        }
+    }
+}
---
+++ Large Walking Excavator/Program.cs
@@ -0,0 +1,254 @@
+﻿/* created 03/2020 by Lars P
+ * This Script handles the movement of a Machine in the Game Space Engineers.
+ * The Machine is called "Large Walking Excavator" and might at some Point be available to Steam Workshop
+ */
+
+using Sandbox.Game.EntityComponents;
+using Sandbox.ModAPI.Ingame;
+using Sandbox.ModAPI.Interfaces;
+using SpaceEngineers.Game.ModAPI.Ingame;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System;
+using VRage.Collections;
+using VRage.Game.Components;
+using VRage.Game.GUI.TextPanel;
+using VRage.Game.ModAPI.Ingame.Utilities;
+using VRage.Game.ModAPI.Ingame;
+using VRage.Game.ObjectBuilders.Definitions;
+using VRage.Game;
+using VRage;
+using VRageMath;
+
+namespace IngameScript
+{
+    partial class Program : MyGridProgram
+    {
+        public Dictionary<string, Leg> legs;
+        public IMyCockpit cockpit;
+        Dictionary<string, string> customData;
+
+        public LegMovement activeMovementAction;
+
+        public class Leg
+        {
+            public Dictionary<string, IMyMotorStator> rotors;
+            public Dictionary<string, IMyPistonBase> pistons;
+            public IMyLandingGear landingGear;
+
+            public Leg(IMyGridTerminalSystem gts, string id)  //id will here be "Left" or "Right" but could e.g. also be numerals
+            {
+                this.rotors = new Dictionary<string, IMyMotorStator>()
+                {
+                    ["yaw leg"] = gts.GetBlockWithName($"LWE Walk Rotor {id} 1 (Yaw Leg)") as IMyMotorStator,
+                    ["yaw body"] = gts.GetBlockWithName($"LWE Walk Rotor {id} 2 (Yaw Body)") as IMyMotorStator,
+                    ["pitch leg"] = gts.GetBlockWithName($"LWE Walk Rotor {id} 3 (Pitch Leg)") as IMyMotorStator,
+                    ["pitch body"] = gts.GetBlockWithName($"LWE Walk Rotor {id} 4 (Pitch Body)") as IMyMotorStator,
+                    ["roll leg"] = gts.GetBlockWithName($"LWE Walk Rotor {id} 5 (Roll Leg)") as IMyMotorStator,
+                    ["roll body"] = gts.GetBlockWithName($"LWE Walk Rotor {id} 6 (Roll Body)") as IMyMotorStator
+                };
+                this.pistons = new Dictionary<string, IMyPistonBase>()
+                {
+                    ["forward"] = gts.GetBlockWithName($"LWE Walk Piston {id} 1 (Forward)") as IMyPistonBase,
+                    ["down"] = gts.GetBlockWithName($"LWE Walk Piston {id} 2 (Down)") as IMyPistonBase
+                };
+                this.landingGear = gts.GetBlockWithName($"LWE Landing Gear {id}") as IMyLandingGear;
+            }
+        
+            public bool checkValidity()
+            {
+                return !(!this.rotors.Any() || !this.pistons.Any() || this.landingGear == null);  // true means valid
+            }
+        }
+
+        public Program()
+        {
+            IMyGridTerminalSystem gts = GridTerminalSystem;
+
+            //init mechanical blocks
+            this.legs = new Dictionary<string, Leg>()
+            {
+                ["right"] = new Leg(gts, "Right"),
+                ["left"] = new Leg(gts, "Left")
+            };
+            this.cockpit = gts.GetBlockWithName("LWE Cockpit") as IMyCockpit;
+            
+            this.activeMovementAction = null;
+
+            if (!(this.legs["right"].checkValidity() && this.legs["left"].checkValidity() && this.cockpit != null))
+            {
+                Echo("one ore more of the expected mechanical components not found!");
+            }
+            else
+            {
+                Echo("all set up!");
+            }
+        }
+
+        public void Save()
+        {
+            // Called when the program needs to save its state. Use
+            // this method to save your state to the Storage field
+            // or some other means. 
+            // 
+            // This method is optional and can be removed if not
+            // needed.
+        }
+
+        public void Main(string argument, UpdateType updateSource)
+        {
+            if (argument != "")
+            {
+                if (argument == "start")
+                {
+                    Runtime.UpdateFrequency = UpdateFrequency.Update1;
+                }
+                else if (argument == "stop")
+                {
+                    Runtime.UpdateFrequency = UpdateFrequency.None;
+                    this.activeMovementAction = null;
+                }
+            }
+
+            //is there an active action? then just do this
+            if (this.activeMovementAction != null)
+            {
+                if (!this.activeMovementAction.Update())  //this continues the action and returns false on finish
+                {
+                    //active action finished
+                    this.activeMovementAction = null;
+                    ReadCustomData();
+                    if (customData["walkState"] == "11")
+                    {
+                        customData["walkState"] = "1";
+
+                        // switch Legs
+                        var activeLeg = customData["walkActiveLeg"];
+                        customData["walkActiveLeg"] = customData["walkInactiveLeg"];
+                        customData["walkInactiveLeg"] = activeLeg;
+                    }
+                    else
+                    {
+                        customData["walkState"] = (Convert.ToInt32(customData["walkState"]) + 1).ToString();
+                    }
+                    WriteCustomData();
+
+                    //Runtime.UpdateFrequency = UpdateFrequency.None;
+                }
+            }
+            else
+            {
+                // choose and set up new active movement action
+                ReadCustomData();
+                var activeLeg = this.legs[customData["walkActiveLeg"]];
+                var inactiveLeg = this.legs[customData["walkInactiveLeg"]];
+                Echo("walk state : " + customData["walkActiveLeg"] + " " + customData["walkState"]);
+
+                switch (Convert.ToInt32(customData["walkState"]))
+                {
+                    case 1:
+
+                        this.activeMovementAction = new RotorPositionSet(activeLeg.rotors["yaw leg"],
+                            activeLeg.rotors["yaw body"].Angle + Convert.ToDouble(customData["addYaw"]));
+                        break;
+                    case 2:  // code here a bit dirty hardcoded... so just look away or sth
+                        var pitch = GetPitch() - Convert.ToDouble(customData["setPitch"]);
+                        if (customData["walkActiveLeg"] == "right")
+                        {
+                            pitch *= -1;
+                        }
+                        this.activeMovementAction = new RotorPositionSet(activeLeg.rotors["pitch leg"],
+                            activeLeg.rotors["pitch body"].Angle + pitch);
+                        break;
+                    case 3:
+                        this.activeMovementAction = new PistonAndLandingGearExtendAndLock(activeLeg.pistons["down"], activeLeg.landingGear);
+                        break;
+                    case 4:
+                        this.activeMovementAction = new UnlockSafe(inactiveLeg.landingGear, activeLeg.landingGear);
+                        break;
+                    case 5:
+                        this.activeMovementAction = new PistonMoveFull(inactiveLeg.pistons["down"], false);
+                        break;
+                    case 6:
+                        this.activeMovementAction = new PistonMoveFull(inactiveLeg.pistons["forward"], false);
+                        break;
+                    case 7:
+                        this.activeMovementAction = new RotorPositionSet(inactiveLeg.rotors["roll leg"],
+                            inactiveLeg.rotors["roll body"].Angle + 0);
+                        break;
+                    case 8:
+                        this.activeMovementAction = new RotorPositionSet(activeLeg.rotors["yaw body"],
+                            -1 * (activeLeg.rotors["yaw leg"].Angle + 0));
+                        break;
+                    case 9:
+                        this.activeMovementAction = new RotorPositionSet(activeLeg.rotors["pitch body"],
+                            -1 * (activeLeg.rotors["pitch leg"].Angle + 0));
+                        break;
+                    case 10:
+                        this.activeMovementAction = new RotorPositionSet(activeLeg.rotors["roll body"],
+                            -1 * (activeLeg.rotors["roll leg"].Angle + GetRoll()));
+                        break;
+                    case 11:
+                        this.activeMovementAction = new PistonMoveFull(activeLeg.pistons["forward"], true);
+                        break;
+                }
+            }
+        }
+
+        private double GetPitch()
+        {
+            var gravityInCockpitCoordinates = Vector3D.TransformNormal(this.cockpit.GetNaturalGravity(), MatrixD.Transpose(this.cockpit.WorldMatrix));
+            return Math.Atan(gravityInCockpitCoordinates.Z / gravityInCockpitCoordinates.Y);  // positive means pitch up
+        }
+
+        private double GetRoll()
+        {
+            var gravityInCockpitCoordinates = Vector3D.TransformNormal(this.cockpit.GetNaturalGravity(), MatrixD.Transpose(this.cockpit.WorldMatrix));
+            return Math.Atan(gravityInCockpitCoordinates.X / gravityInCockpitCoordinates.Y);  // positive means roll left
+        }
+
+        private void ReadCustomData()
+        {
+            // read the command blocks custom data field
+            this.customData = new Dictionary<string, string>();
+            foreach (string dataLine in Me.CustomData.Split('\n'))
+            {
+                var splittedDataLine = dataLine.Split(new char[] { '=' }, 2);
+                if (splittedDataLine.Length == 1)
+                {
+                    customData[splittedDataLine[0]] = "";
+                }
+                else
+                {
+                    customData[splittedDataLine[0]] = splittedDataLine[1];
+                }
+            }
+        }
+
+        private void WriteCustomData()
+        {
+            // write the command blocks custom data field
+            //string customDataAccumulated = "";
+            //foreach (KeyValuePair<string, string> dataPair in this.customData)
+            //{
+            //    customDataAccumulated = String.Join("\n", customDataAccumulated, String.Join("=", dataPair.Key, dataPair.Value));
+            //}
+            //Me.CustomData = customDataAccumulated;
+            Me.CustomData =
+                "### Settings ###\n" +
+                "# Turns by this angle every walk step\n" +
+                $"addYaw={customData["addYaw"]}\n" +
+                "# keeps This Pitch relative to Gravity\n" +
+                $"setPitch={customData["setPitch"]}\n" +
+                "\n" +
+                "\n" +
+                "### States handled by Script ###\n" +
+                $"walkActiveLeg={customData["walkActiveLeg"]}\n" +
+                $"walkInactiveLeg={customData["walkInactiveLeg"]}\n" +
+                $"walkState={customData["walkState"]}\n" +
+                "";
+        }
+    }
+}
